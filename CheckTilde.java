f3a016a199e86eb4ec137804ecbf8ad73357a817
tongrenlu114514 <wdxzs1985@163.com>
2026-09-08 01:22:09 +0800

    删垃圾

--- CheckTilde.java
+++
@@ -1,8 +0,0 @@
-public class CheckTilde {
-    public static void main(String[] args) throws Exception {
-        java.nio.file.Files.writeString(
-            java.nio.file.Paths.get('check.txt'),
-            Integer.toHexString(args[0].codePointAt(0)) + ' ' + Integer.toHexString(args[1].codePointAt(0))
-        );
-    }
-}
